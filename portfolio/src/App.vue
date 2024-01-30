a9359521317e6caf7a9974df4a4ddd1e946056eb
Zyber Loshka <75445321+Zyb3rWolfi@users.noreply.github.com>
2024-01-30 22:02:55 +0000

    init

---
+++ portfolio/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { initFlowbite } from 'flowbite'
+import { onMounted } from 'vue'
+import navbar from './components/navbar.vue';
+import mainView from './views/main-view.vue'
+
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+
+<template>
+  <navbar />
+  <mainView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ portfolio/src/views/main-view.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <div class=" flex mt-28">
+        <div class=" mx-auto text-center">
+            <h1 class=" text-4xl">Hello Im Anton</h1>
+            <p class=" text-md mt-2 text-neutral-400">Im a software engineer that focuses on front end development with <br> web applications and i can occasionally do back end too.</p>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
